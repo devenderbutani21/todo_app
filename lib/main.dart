b1272848813485c32d5c4b1dcc22e3773bc523e7
devenderbutani21 <devenderbutani21@gmail.com>
2020-06-12 20:18:09 +0530

    Todo App

---
+++ lib/main.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Todo List',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        canvasColor: Colors.orangeAccent.shade100,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: TodoList(),
+    );
+  }
+}
+
+class TodoList extends StatefulWidget{
+  @override
+  createState() => TodoListState();
+}
+
+class TodoListState extends State<TodoList> {
+
+  List<String> _todoItems = [];
+  // Add to list and build-list section
+  void _addTodoItem(String task) {
+    if(task.length > 0 ) {
+      setState(() {
+        _todoItems.add(task);
+      });
+    }
+  }
+
+  Widget _buildTodoList() {
+    return ListView.builder(
+        itemBuilder: (context, index) {
+          if(index < _todoItems.length) {
+            return _buildTodoItem( _todoItems[index], index);
+          }
+        },
+    );
+  }
+  
+  Widget _buildTodoItem(String todoText, int index) {
+    return ListTile(
+     title: Text(todoText),
+      onTap: () => _promptRemoveTodoItem(index),
+    );
+  }
+  
+  void _pushAddTodoScreen() {
+    Navigator.of(context).push (
+      MaterialPageRoute(
+        builder: (context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Add a new task'),
+            ),   
+            body: TextField(
+              autofocus: true,
+              onSubmitted: (val) {
+                _addTodoItem(val);
+                Navigator.pop(context);
+              },
+              decoration: InputDecoration(
+                hintText: 'Enter Something to do',
+                contentPadding: const EdgeInsets.all(16.0)
+              ),
+            )
+          );
+        })
+    );
+  }
+  
+  // Remove from list section
+  void _removeTodoItem(int index) {
+    setState(() => _todoItems.removeAt(index));
+  }
+  
+  void _promptRemoveTodoItem(int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog (
+          title: Text ('Mark "${_todoItems[index]} as done?"'),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('CANCEL'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            FlatButton(
+              child: Text('MARK AS DONE'),
+              onPressed: () {
+                _removeTodoItem(index);
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      }
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+        title: Text('Todo List'),
+    ),
+      body: _buildTodoList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _pushAddTodoScreen,
+        tooltip: 'Add task',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
