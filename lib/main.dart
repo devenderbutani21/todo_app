8532f5677070cd88ee42d758c5e9c50f26846780
devenderbutani21 <devenderbutani21@gmail.com>
2020-07-25 18:31:06 +0530

    Implementing Hive Database

--- lib/main.dart
+++ lib/main.dart
@@ -1,234 +1,166 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:intl/intl.dart';
-import 'package:path_provider/path_provider.dart' as path_provider;
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import 'todo_model.dart';
+
+const String todoBoxName = "todo";
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final appDocumentDir = await path_provider.getApplicationDocumentsDirectory();
+  final document = await getApplicationDocumentsDirectory();
+  Hive.init(document.path);
+  Hive.registerAdapter(TodoModelAdapter());
+  await Hive.openBox<TodoModel>(todoBoxName);
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Todo List',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
-        canvasColor: Colors.white,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primarySwatch: Colors.blue,
       ),
-      home: TodoList(),
+      home: MyHomePage(),
     );
   }
 }
 
-
-class TodoList extends StatefulWidget{
+class MyHomePage extends StatefulWidget {
   @override
-  createState() => TodoListState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class TodoListState extends State<TodoList> {
+enum TodoFilter { ALL, COMPLETED, INCOMPLETE }
 
-  List<String> _todoItems = [];
-  List<String> _dateTimeItems = [];
-  List<String> myList = [];
-  // Add to list and build-list section
+class _MyHomePageState extends State<MyHomePage> {
+  Box<TodoModel> todoBox;
 
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController detailController = TextEditingController();
 
+  TodoFilter filter = TodoFilter.ALL;
 
-
-  void _addTodoItem(String task, String date) {
-    if(task.length > 0 ) {
-      setState(() {
-        _todoItems.add(task);
-        _dateTimeItems.add(date);
-      });
-    }
-  }
-
-  Widget _buildTodoList() {
-    return ListView.builder(
-        itemBuilder: (context, index) {
-          if(index < _todoItems.length) {
-            return _buildTodoItem( _todoItems[index], index, _dateTimeItems[index]);
-          }
-        },
-    );
-  }
-
-  Widget _buildTodoItem(String todoText, int index, String dateText) {
-    return Card (
-//      shape: RoundedRectangleBorder(
-//        borderRadius: BorderRadius.circular(5.0),
-//      ), // for rounding the card
-      color: Colors.lightBlueAccent,
-      child: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-                Colors.blue,
-                Colors.blueAccent,
-            ],
-          ),
-        ),
-        child: ListTile(
-          title: Text(
-              dateText,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 12,
-            ),
-          ),
-          subtitle: Text(
-              todoText,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 24,
-              color: Colors.black,
-            ),
-          ),
-          onTap: () => _promptRemoveTodoItem(index),
-        ),
-      ),
-    );
-  }
-
-  void _pushAddTodoScreen() {
-    Navigator.of(context).push (
-      MaterialPageRoute(
-        builder: (context) {
-          return Scaffold(
-            appBar: AppBar(
-              title: Text(
-                'Add a new task',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 26,
-                  fontFamily: 'Nunito Sans',
-                ),
-              ),
-            ),
-            body: TextField(
-              autofocus: true,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 24,
-              ),
-              onSubmitted: (val) {
-                DateTime now = DateTime.now();
-                String formattedDate = DateFormat('dd/MM/yyyy').format(now);
-                _addTodoItem(val, formattedDate,);
-                _save();
-                Navigator.pop(context);
-              },
-              decoration: InputDecoration(
-                hintText: 'Enter Something to do',
-                  border: InputBorder.none,
-                  contentPadding: const EdgeInsets.all(16.0)
-              ),
-            )
-          );
-        })
-    );
-  }
-
-  // Save in Shared Preference
-  _save() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      myList  = (prefs.getStringList('mylist') ?? List<String>()) ;
-    });
-//    List<int> myOriginaList = myList.map((i)=> int.parse(i)).toList();
-    print('Your list  $myList');
-    await prefs.setStringList('mylist', _todoItems);
-  }
-
-  // Remove from list section
-  void _removeTodoItem(int index) {
-    setState(() {
-      _todoItems.removeAt(index);
-      _dateTimeItems.removeAt(index);
-    });
-  }
-
-  void _promptRemoveTodoItem(int index) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog (
-          title: Text (
-            'Mark "${_todoItems[index]}" as done?',
-            style: TextStyle(
-              fontFamily: 'Nunito Sans',
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            )
-          ),
-          actions: <Widget>[
-            FlatButton(
-              child: Text(
-                'CANCEL',
-                  style: TextStyle(
-                    fontFamily: 'Nunito Sans',
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  )
-              ),
-              onPressed: () => Navigator.of(context).pop(),
-            ),
-            FlatButton(
-              child: Text(
-                  'MARK AS DONE',
-                   style: TextStyle(
-                    fontFamily: 'Nunito Sans',
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  )
-              ),
-              onPressed: () {
-                _removeTodoItem(index);
-                Navigator.of(context).pop();
-              },
-            )
-          ],
-        );
-      }
-    );
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    todoBox = Hive.box<TodoModel>(todoBoxName);
   }
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-    ]);
     return Scaffold(
-        appBar: AppBar(
-        title: Text('Todo List',
-        style: TextStyle(
-          fontSize: 26,
-          fontFamily: 'Nunito Sans',
-          fontWeight: FontWeight.w700,
-        ),
-        ),
-    ),
-      body: _buildTodoList(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _pushAddTodoScreen,
-        tooltip: 'Add task',
-        child: Icon(Icons.add),
+      appBar: AppBar(
+        title: Text("Hive Todo"),
+        actions: <Widget>[
+          PopupMenuButton<String>(
+            onSelected: (value) {
+              ///Todo : Take action accordingly
+              ///
+              if (value.compareTo("All") == 0) {
+                setState(() {
+                  filter = TodoFilter.ALL;
+                });
+              } else if (value.compareTo("Completed") == 0) {
+                setState(() {
+                  filter = TodoFilter.COMPLETED;
+                });
+              } else {
+                setState(() {
+                  filter = TodoFilter.INCOMPLETE;
+                });
+              }
+            },
+            itemBuilder: (BuildContext context) {
+              return ["All", "Completed", "Incomplete"].map((option) {
+                return PopupMenuItem(
+                  value: option,
+                  child: Text(option),
+                );
+              }).toList();
+            },
+          )
+        ],
       ),
+
+      body: Column(
+        children: <Widget>[
+          ValueListenableBuilder(
+            valueListenable: todoBox.listenable(),
+            builder: (context, Box<TodoModel> todos, _) {
+              List<int> keys = todos.keys.cast<int>().toList();
+              return ListView.separated(
+                itemBuilder: (_, index) {
+                  final int key = keys[index];
+
+                  final TodoModel todo = todos.get(index);
+
+                  return ListTile(
+
+                  );
+                },
+                separatorBuilder: (_, index) => Divider(),
+                itemCount: keys.length,
+               );
+            },
+          )
+        ],
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+              context: context,
+              child: Dialog(
+                  child: Container(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(hintText: "Title"),
+                      controller: titleController,
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(hintText: "Detail"),
+                      controller: detailController,
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    FlatButton(
+                      child: Text("Add Todo"),
+                      onPressed: () {
+                        ///Todo : Add Todo in hive
+                        final String title = titleController.text;
+                        final String detail = detailController.text;
+
+                        TodoModel todo = TodoModel(
+                            title: title, detail: detail, isCompleted: false);
+
+                        todoBox.add(todo);
+
+                        Navigator.pop(context);
+                      },
+                    )
+                  ],
+                ),
+              )));
+        },
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
-
 
 // For UI testing
 //import 'package:flutter/material.dart';
