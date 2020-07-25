47d4a756d3cf37a740cd6aa72cd5764a36966115
devenderbutani21 <devenderbutani21@gmail.com>
2020-07-25 23:24:38 +0530

    Hive Functionality Added

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,6 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -55,109 +54,166 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Hive Todo"),
-        actions: <Widget>[
-          PopupMenuButton<String>(
-            onSelected: (value) {
-              ///Todo : Take action accordingly
-              ///
-              if (value.compareTo("All") == 0) {
-                setState(() {
-                  filter = TodoFilter.ALL;
-                });
-              } else if (value.compareTo("Completed") == 0) {
-                setState(() {
-                  filter = TodoFilter.COMPLETED;
-                });
-              } else {
-                setState(() {
-                  filter = TodoFilter.INCOMPLETE;
-                });
-              }
-            },
-            itemBuilder: (BuildContext context) {
-              return ["All", "Completed", "Incomplete"].map((option) {
-                return PopupMenuItem(
-                  value: option,
-                  child: Text(option),
-                );
-              }).toList();
-            },
-          )
-        ],
-      ),
-
-      body: Column(
-        children: <Widget>[
-          ValueListenableBuilder(
-            valueListenable: todoBox.listenable(),
-            builder: (context, Box<TodoModel> todos, _) {
-              List<int> keys = todos.keys.cast<int>().toList();
-              return ListView.separated(
-                itemBuilder: (_, index) {
-                  final int key = keys[index];
-
-                  final TodoModel todo = todos.get(index);
-
-                  return ListTile(
-
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Hive Todo"),
+          actions: <Widget>[
+            PopupMenuButton<String>(
+              onSelected: (value) {
+                if (value.compareTo("All") == 0) {
+                  setState(() {
+                    filter = TodoFilter.ALL;
+                  });
+                } else if (value.compareTo("Completed") == 0) {
+                  setState(() {
+                    filter = TodoFilter.COMPLETED;
+                  });
+                } else {
+                  setState(() {
+                    filter = TodoFilter.INCOMPLETE;
+                  });
+                }
+              },
+              itemBuilder: (BuildContext context) {
+                return ["All", "Completed", "Incomplete"].map((option) {
+                  return PopupMenuItem(
+                    value: option,
+                    child: Text(option),
                   );
-                },
-                separatorBuilder: (_, index) => Divider(),
-                itemCount: keys.length,
-               );
-            },
-          )
-        ],
-      ),
+                }).toList();
+              },
+            ),
+          ],
+        ),
 
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog(
+        body: Column(
+          children: <Widget>[
+            ValueListenableBuilder(
+              valueListenable: todoBox.listenable(),
+              builder: (context, Box<TodoModel> todos, _) {
+                List<int> keys;
+
+                if (filter == TodoFilter.ALL) {
+                  keys = todos.keys.cast<int>().toList();
+                } else if (filter == TodoFilter.COMPLETED) {
+                  keys = todos.keys
+                      .cast<int>()
+                      .where((key) => todos.get(key).isCompleted)
+                      .toList();
+                } else {
+                  keys = todos.keys
+                      .cast<int>()
+                      .where((key) => !todos.get(key).isCompleted)
+                      .toList();
+                }
+
+                return ListView.separated(
+                  itemBuilder: (_, index) {
+                    final int key = keys[index];
+                    final TodoModel todo = todos.get(key);
+
+                    return ListTile(
+                      title: Text(
+                        todo.title,
+                        style: TextStyle(fontSize: 24),
+                      ),
+                      subtitle: Text(
+                        todo.detail,
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      trailing: Icon(
+                        Icons.check,
+                        color: todo.isCompleted ? Colors.green : Colors.red,
+                      ),
+                      onTap: () {
+                        showDialog(
+                          context: context,
+                          child: Dialog(
+                            child: Container(
+                              padding: EdgeInsets.all(16),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  FlatButton(
+                                    child: Text("Mark as completed"),
+                                    onPressed: () {
+                                      TodoModel mTodo = TodoModel(
+                                        title: todo.title,
+                                        detail: todo.detail,
+                                        isCompleted: true,
+                                      );
+                                      todoBox.put(key, mTodo);
+                                      Navigator.pop(context);
+                                    },
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                  separatorBuilder: (_, index) => Divider(),
+                  itemCount: keys.length,
+                  shrinkWrap: true,
+                );
+              },
+            ),
+          ],
+        ),
+
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showDialog(
               context: context,
               child: Dialog(
-                  child: Container(
-                padding: EdgeInsets.all(16),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(hintText: "Title"),
-                      controller: titleController,
-                    ),
-                    SizedBox(
-                      height: 8,
-                    ),
-                    TextField(
-                      decoration: InputDecoration(hintText: "Detail"),
-                      controller: detailController,
-                    ),
-                    SizedBox(
-                      height: 8,
-                    ),
-                    FlatButton(
-                      child: Text("Add Todo"),
-                      onPressed: () {
-                        ///Todo : Add Todo in hive
-                        final String title = titleController.text;
-                        final String detail = detailController.text;
+                child: Container(
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(hintText: "Title"),
+                        controller: titleController,
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(hintText: "Detail"),
+                        controller: detailController,
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      FlatButton(
+                        child: Text("Add Todo"),
+                        onPressed: () {
+                          final String title = titleController.text;
+                          final String detail = detailController.text;
 
-                        TodoModel todo = TodoModel(
-                            title: title, detail: detail, isCompleted: false);
+                          TodoModel todo = TodoModel(
+                            title: title,
+                            detail: detail,
+                            isCompleted: false,
+                          );
 
-                        todoBox.add(todo);
+                          todoBox.add(todo);
 
-                        Navigator.pop(context);
-                      },
-                    )
-                  ],
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ],
+                  ),
                 ),
-              )));
-        },
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+              ),
+            );
+          },
+          child: Icon(Icons.add),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
